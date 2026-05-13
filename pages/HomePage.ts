f4feaf484f1a653f2f47692a34741853d6803f39
Wara Rojas <wara.rojas.u@gmail.com>
2026-05-13 01:04:32 -0400

    removint comented lines

--- pages/HomePage.ts
+++ pages/HomePage.ts
@@ -1,12 +1,4 @@
 import { Locator, Page } from "@playwright/test";
-import { config } from '../config/config'
-
-//const env = process.env.TEST_ENV || 'dev'
-//const settings = config[env as keyof typeof config]
-
-
-import { env } from '../config/environments'
-
 
 export class HomePage{
     page: Page;
--- pages/ProfilePage.ts
+++ pages/ProfilePage.ts
@@ -1,4 +1,5 @@
-import {Locator, Page} from "@playwright/test"
+import { Locator, Page } from "@playwright/test";
+
 export class ProfilePage{
     page: Page;
     goToBookStoreButton: Locator;
