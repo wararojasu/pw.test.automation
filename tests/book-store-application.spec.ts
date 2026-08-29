b3cb6bf221df0950f49c3cdf8c6a617936a6da23
Wara Rojas <wara.rojas.u@gmail.com>
2026-08-29 12:31:37 -0400

    adding type data return and asertion

--- tests/book-store-application.spec.ts
+++ tests/book-store-application.spec.ts
@@ -1,16 +1,24 @@
-import {test} from "@playwright/test";
-import {HomePage} from "../pages/HomePage";
+import { test, expect } from "@playwright/test";
+import { HomePage } from "../pages/HomePage";
 import { LoginPage } from "../pages/LoginPage";
 import { ProfilePage} from "../pages/ProfilePage";
 import { BookStorePage} from "../pages/BookStorePage";
 
 test.describe('Book Store Tests', () => {
-    test("user is able to add a book in his collection",async({page})=>{
-        const BOOK_NAME = 'Understanding ECMAScript';
-        const homePage = new HomePage(page);
-        const loginPage = new LoginPage(page);
-        const profilePage = new ProfilePage(page);
-        const bookStorePage = new BookStorePage(page);
+    const BOOK_NAME = 'Understanding ECMAScript 6';
+    let homePage: HomePage;
+    let loginPage: LoginPage;
+    let profilePage: ProfilePage;
+    let bookStorePage: BookStorePage;
+
+    test.beforeEach('setup', async({page})=>{
+        homePage = new HomePage(page);
+        loginPage = new LoginPage(page);
+        profilePage = new ProfilePage(page);
+        bookStorePage = new BookStorePage(page);
+    });
+
+    test('user is able to add a book in his collection', async()=>{
         await homePage.gotToApplication();
         await homePage.navigateToBookApplication();
         await homePage.goToLogin();
@@ -18,5 +26,11 @@ test.describe('Book Store Tests', () => {
         await profilePage.goToBookStore();
         await bookStorePage.selectBook(BOOK_NAME);
         await bookStorePage.addBookToTheCollection();
+        await bookStorePage.goToProfile();
+        await expect(bookStorePage.getBookNameAdded(BOOK_NAME)).toBeVisible();
+    });
+
+    test.afterEach('clean data', async() => {       
+        await profilePage.deleteBookFromTheCollection();
     });
 });
